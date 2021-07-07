452e24d0f7a4ae1f9d8dd485ce1eedc6558d9c22
Fox <72220905+Xadriek@users.noreply.github.com>
2021-07-08 01:40:33 +0200

    create

---
+++ ProgettoNoleggioMonopattini/src/main/java/it/uniroma3/siw/rentalev/repository/PartnerWalletRepository.java
@@ -0,0 +1,12 @@
+package it.uniroma3.siw.rentalev.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.siw.rentalev.model.User;
+import it.uniroma3.siw.rentalev.model.Wallet;
+
+public interface PartnerWalletRepository extends CrudRepository<Wallet, Long>{
+	
+	public Wallet findByOwner(User owner);
+
+}
