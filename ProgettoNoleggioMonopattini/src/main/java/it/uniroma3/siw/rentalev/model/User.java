e1e10c7e1e5bbf90c0c8506932f80250f4a6ab46
Fox <72220905+Xadriek@users.noreply.github.com>
2021-07-08 20:57:05 +0200

    .

--- ProgettoNoleggioMonopattini/src/main/java/it/uniroma3/siw/rentalev/model/User.java
+++ ProgettoNoleggioMonopattini/src/main/java/it/uniroma3/siw/rentalev/model/User.java
@@ -18,7 +18,6 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-import lombok.Data;
 
 @Entity
 @Table(	name = "users", 
--- ProgettoNoleggioMonopattini/src/main/java/it/uniroma3/siw/rentalev/repository/WalletRepository.java
+++ ProgettoNoleggioMonopattini/src/main/java/it/uniroma3/siw/rentalev/repository/WalletRepository.java
@@ -3,7 +3,7 @@ package it.uniroma3.siw.rentalev.repository;
 import org.springframework.data.repository.CrudRepository;
 
 
-import it.uniroma3.siw.rentalev.model.UserInformation;
+
 import it.uniroma3.siw.rentalev.model.Wallet;
 
 public interface WalletRepository extends CrudRepository<Wallet, Long>{
