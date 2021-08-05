cd75f66826493e902a47836afd401197829f564b
Fox <72220905+Xadriek@users.noreply.github.com>
2021-08-05 18:07:31 +0200

    .

--- ProgettoNoleggioMonopattini/front_end/src/view/BoardCustomer.vue
+++ ProgettoNoleggioMonopattini/front_end/src/view/BoardCustomer.vue
@@ -3,13 +3,25 @@
     <header class="jumbotron">
       <h3>{{content}}</h3>
     </header>
+    <div>
+      <div class="mb-3">
+      <b-button v-b-toggle.my-collapse>Toggle Collapse</b-button>
+    </div>
+     <b-collapse id="my-collapse">
+      <b-card title="Collapsible card">
+        <add-rent/>
+      </b-card>
+    </b-collapse>
+    </div>
   </div>
 </template>
 
 <script>
+import AddRent from '../components/AddRent.vue';
 import UserService from '../services/user.service';
 
 export default {
+  components: { AddRent },
   name: 'customer',
   data() {
     return {
