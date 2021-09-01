aa3daf8865fa550178651a74ef67f70d1add0764
tororoma <antonio.albano70@gmail.com>
2021-09-01 17:31:02 +0200

    Update BoardAdmin.vue

--- ProgettoNoleggioMonopattini/front_end/src/view/BoardAdmin.vue
+++ ProgettoNoleggioMonopattini/front_end/src/view/BoardAdmin.vue
@@ -46,41 +46,29 @@
           <b-card no-body>
             <b-tabs card>
               <b-tab no-body title="Clienti">
-                <b-table :items="customers" striped responsive="sm">
-                  <template #cell(show_details)="row">
-                    <b-button size="sm" @click="row.toggleDetails" class="mr-2">
-                      {{ row.detailsShowing ? "Hide" : "Show" }} Details
-                    </b-button>
-
-                    <!-- As `row.showDetails` is one-way, we call the toggleDetails function on @change -->
-                    <b-form-checkbox
-                      v-model="row.detailsShowing"
-                      @change="row.toggleDetails"
-                    >
-                      Details via check
-                    </b-form-checkbox>
-                  </template>
-
-                  <template #row-details="row">
-                    <b-card>
-                      <b-row class="mb-2">
-                        <b-col sm="3" class="text-sm-right"><b>Age:</b></b-col>
-                        <b-col>{{ row.item.age }}</b-col>
-                      </b-row>
-
-                      <b-row class="mb-2">
-                        <b-col sm="3" class="text-sm-right"
-                          ><b>Is Active:</b></b-col
-                        >
-                        <b-col>{{ row.item.isActive }}</b-col>
-                      </b-row>
-
-                      <b-button size="sm" @click="row.toggleDetails"
-                        >Hide Details</b-button
+                <b-container class="bv-row">
+                  <b-row class="my-1">
+                    <b-col v-for="type in types" :key="type">
+                      <label :for="`type-${type}`">
+                        <code>{{ type }}</code
+                        >:</label
                       >
-                    </b-card>
-                  </template>
-                </b-table>
+                      <b-form-input :id="`type-${type}`"></b-form-input>
+                      </b-col>
+                      <b-col>
+                      <label >  Active</label                      >
+                      <b-form-checkbox
+                        v-model="checked"
+                        name="check-button"
+                        switch
+                      >
+                        Abilitazione 
+                      </b-form-checkbox>
+                    </b-col>
+                  </b-row>
+                </b-container>
+
+                <b-table :items="customers" striped hover> </b-table>
               </b-tab>
 
               <b-tab no-body title="Partner">
@@ -120,6 +108,17 @@ export default {
   data() {
     return {
       checked: false,
+      types: [
+        "id",
+        "Name",
+        "Surname",
+        "Telephon",
+        "Address",
+        "Customer Wallet",
+        "Rent",
+        "Email",
+        "Username",
+      ],
       customers: [],
       partners: [],
       rents: [],
@@ -128,7 +127,6 @@ export default {
       countTransation: 0,
       countRentOngoing: 0,
       count: 0,
-      
     };
   },
   mounted() {
